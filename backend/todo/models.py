0d18d2b08b9a2afabb1cbfb66bd095d805edfda9
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-04-14 22:11:58 +0200

    created the model Todo

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Models):
+    title = models.Charfiled(max_length=120)
+    description= models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
