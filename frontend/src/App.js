6336ed3124da0c9b9955dd0899e2786004165222
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-04-16 00:47:18 +0200

    created new App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from "react"
+
+const todoItems = [
+  {
+    id: 1,
+    title: "Go to Market",
+    description: "Buy ingredients to prepare dinner",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Study",
+    description: "Read Algebra and History textbook for the upcoming test",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Sammy's books",
+    description: "Go to library to return Sammy's books",
+    completed: true,
+  },
+  {
+    id: 4,
+    title: "Article",
+    description: "Write article on how to use Django with React",
+    completed: false,
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [viewCompleted, setViewCompleted] = useState(false)
+  const [todoList, setTodoList] = useState(todoItems)
+
+  const displayCompleted = status => {
+    if (status) {
+      return setViewCompleted(true)
+    }
+
+    return setViewCompleted(false)
+  }
+
+  const renderTabList = () => {
+    return (
+      <div>
+        <span
+          className=''
+          onClick={() => displayCompleted(true)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Complete
+        </span>
+        <span
+          className=''
+          onClick={() => displayCompleted(false)}
+        >
+          Incomplete
+        </span>
+      </div>
+    )
+  }
+
+  const renderItems = () => {
+    const newItems = todoList.filter(item => item.completed = viewCompleted)
+
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className=''
+      >
+        <span
+          className=''
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            className=''
+          >
+            Edit
+          </button>
+          <button
+            className=''
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ))
+  }
+  return (
+    <main className=''>
+      <h1 className=''>Todo App</h1>
+      <div>
+        <button>
+          Add task
+        </button>
+        {renderTabList()}
+        <ul>
+          {renderItems()}
+        </ul>
+      </div>
+    </main>
+  )
 }
 
 export default App;
