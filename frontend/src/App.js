a94eca6d0b0ea8d9131bdc1df5488ef3fa6f30a4
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-04-22 23:26:15 +0200

    installed axios, removed hard coded todoItems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,12 @@
 import './App.css'
 import { useState } from "react"
+import axios from 'axios'
 
 import Modal from './components/Modal';
 
-const todoItems = [
-  {
-    id: 1,
-    title: "Go to Market",
-    description: "Buy ingredients to prepare dinner",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "Study",
-    description: "Read Algebra and History textbook for the upcoming test",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Sammy's books",
-    description: "Go to library to return Sammy's books",
-    completed: true,
-  },
-  {
-    id: 4,
-    title: "Article",
-    description: "Write article on how to use Django with React",
-    completed: false,
-  },
-];
-
 function App() {
   const [viewCompleted, setViewCompleted] = useState(false)
-  const [todoList, setTodoList] = useState(todoItems)
+  const [todoList, setTodoList] = useState([])
   const [modal, setModal] = useState(false)
   const [activeItem, setActiveItem] = useState({
     title: "",
