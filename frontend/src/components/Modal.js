a8e82e3ce6edf5c66dd0cabc6cb320fb6aa66dfd
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-04-23 13:19:07 +0200

    modal bc black and position absolute

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -15,43 +15,45 @@ const Modal = ({activeItem, toggle, handleSubmit}) => {
   const handleChangeComplete = evt => setComplete(evt.target.checked)
 
   return (
-    <section className="modal">
-      <div className='modal-header '>
-        <h2>Todo Item</h2>
-        <span onClick={toggle}>✖</span>
+    <section className="modal-container">
+      <div className="modal">
+        <div className='modal-header '>
+          <h2>Todo Item</h2>
+          <span onClick={toggle}>✖</span>
+        </div>
+        <div className='padding-top'>
+          <label>Title</label><br/>
+          <input
+            className='width margin-y padding'
+            value={activeItem.title ? activeItem.title : title}
+            onChange={evt => handleChangeTitle(evt)}
+          /><br/>
+          <label>Description</label><br/>
+          <input
+            className='width margin-y padding'
+            value={activeItem.description ? activeItem.description : description}
+            onChange={evt => handleChangeDescription(evt)}
+          /><br/>
+          <input
+            className='margin-right'
+            type='checkbox'
+            checked={activeItem.complete ? activeItem.complete : complete}
+            onChange={evt => handleChangeComplete(evt)}/>
+          <label>Completed</label>
+        </div>
+        <input
+          className='modal-btn bc-color-limegreen color-white padding border-radius'
+          type='submit'
+          value='Save'
+          onClick={() => {
+            activeItem.title = title
+            activeItem.description = description
+            activeItem.complete = complete
+            handleSubmit()
+            toggle()
+          }}
+        />
       </div>
-      <div className='padding-top'>
-        <label>Title</label><br/>
-        <input
-          className='width margin-y padding'
-          value={activeItem.title ? activeItem.title : title}
-          onChange={evt => handleChangeTitle(evt)}
-        /><br/>
-        <label>Description</label><br/>
-        <input
-          className='width margin-y padding'
-          value={activeItem.description ? activeItem.description : description}
-          onChange={evt => handleChangeDescription(evt)}
-        /><br/>
-        <input
-          className='margin-right'
-          type='checkbox'
-          checked={activeItem.complete ? activeItem.complete : complete}
-          onChange={evt => handleChangeComplete(evt)}/>
-        <label>Completed</label>
-      </div>
-      <input
-        className='modal-btn bc-color-limegreen color-white padding border-radius'
-        type='submit'
-        value='Save'
-        onClick={() => {
-          activeItem.title = title
-          activeItem.description = description
-          activeItem.complete = complete
-          handleSubmit()
-          toggle()
-        }}
-      />
     </section>
   )
 }
